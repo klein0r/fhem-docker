d1f832665dc0b6140cc6a17cb9313d21d7ab18f2
Matthias Kleine <matthias@klein0r.de>
2017-01-29 19:13:45 +0100

    FHEM mit nofork nicht im Hintergrund starten

--- fhem/start.sh
+++ fhem/start.sh
@@ -6,12 +6,3 @@ port=7072
 
 echo "Starte FHEM"
 perl fhem.pl fhem.cfg
-
-echo "FHEM Update"
-perl fhem.pl 7072 "update all"
-
-echo "FHEM Neustart"
-perl fhem.pl 7072 "shutdown restart"
-
-echo "Alles fertig"
-tail -f /dev/null
